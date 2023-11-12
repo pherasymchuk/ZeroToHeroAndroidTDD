1f8141f3ea6bb9b76d3b59eae0b12d9626e71c27
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2023-11-12 21:33:59 +0200

    Task007 completed using onSaveInstanceState/onRestoreInstanceState

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,34 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var changeButton: Button
+    private lateinit var titleTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        changeButton = findViewById(R.id.changeButton)
+        titleTextView = findViewById(R.id.titleTextView)
+
+        changeButton.setOnClickListener {
+            titleTextView.text = getString(R.string.i_am_an_android_developer)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("key", titleTextView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        titleTextView.text = savedInstanceState.getString("key")
     }
 }
