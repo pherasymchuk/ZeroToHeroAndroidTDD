a61d6eb09f1760de0fc0f62fa797a17f9637004c
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2023-12-03 18:59:43 +0200

    Task012 completed

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,25 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface Count {
+    fun increment(number: String): UiState
+
+    class Base(private val step: Int, private val max: Int) : Count {
+
+        init {
+            if (step < 1) throw IllegalStateException("step should be positive, but was $step")
+            if (max < 1) throw IllegalStateException("max should be positive, but was $max")
+            if (max < step) throw IllegalStateException("max should be more than step")
+        }
+
+        override fun increment(number: String): UiState {
+            val numberAsInt = number.toInt()
+            val result = numberAsInt + step
+
+            return if (result + step <= max) {
+                UiState.Base(result.toString())
+            } else {
+                UiState.Max(result.toString())
+            }
+        }
+    }
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
@@ -7,27 +8,40 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var state: UiState = UiState.Base("0")
+    private val count: Count = Count.Base(2, 4)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-}
 
-interface Count {
-    fun increment(number: String): UiState
-
-    class Base(private val step: Int, private val max: Int) : Count {
-        override fun increment(number: String): UiState {
-            TODO()
+        binding.incrementButton.setOnClickListener {
+            state = count.increment(binding.countTextView.text.toString())
+            state.apply(binding.countTextView, binding.incrementButton)
         }
     }
-}
 
-interface UiState {
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(UI_STATE_KEY, state)
+    }
 
-    class Base(text: String) : UiState
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            state =
+                savedInstanceState.getParcelable(UI_STATE_KEY, UiState::class.java) ?: UiState.Base(
+                    "0"
+                )
+        } else {
+            @Suppress("DEPRECATION")
+            state = savedInstanceState.getParcelable(UI_STATE_KEY) ?: UiState.Base("0")
+        }
+        state.apply(binding.countTextView, binding.incrementButton)
+    }
 
-    class Max(text: String) : UiState
+    companion object {
+        private const val UI_STATE_KEY = "ui-state"
+    }
 }
