3f55fa7b70ec219bc512e9f72392d9d8adfe57d6
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-01-28 21:04:11 +0200

    feat: implement UiState

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,6 +7,7 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var state: UiState
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +18,26 @@ class MainActivity : AppCompatActivity() {
 
 interface UiState {
 
-    data class Base(private val text: String) : UiState
-    data class Min(private val text: String) : UiState
-    data class Max(private val text: String) : UiState
+    fun apply(binding: ActivityMainBinding)
+
+    data class Base(private val text: String) : UiState {
+        override fun apply(binding: ActivityMainBinding) {
+            binding.decrementButton.isEnabled = true
+            binding.incrementButton.isEnabled = true
+        }
+    }
+
+    data class Min(private val text: String) : UiState {
+        override fun apply(binding: ActivityMainBinding) {
+            binding.incrementButton.isEnabled = true
+            binding.decrementButton.isEnabled = false
+        }
+    }
+
+    data class Max(private val text: String) : UiState {
+        override fun apply(binding: ActivityMainBinding) {
+            binding.incrementButton.isEnabled = false
+            binding.decrementButton.isEnabled = false
+        }
+    }
 }
