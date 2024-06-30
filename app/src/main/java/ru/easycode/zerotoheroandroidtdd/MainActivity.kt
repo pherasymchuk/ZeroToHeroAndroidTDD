f08919da6df623884d4658f7f1ee0a07e95a5b5a
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-06-30 15:41:36 +0300

    Solve Task/018-clouddatasource

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,35 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+import ru.easycode.zerotoheroandroidtdd.wrappers.BundleWrapper
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by lazy { (application as ProvideMainViewModel).provide() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.liveData().observe(this) { newUiState: UiState ->
+            newUiState.apply(binding)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        viewModel.saveState(BundleWrapper.Base(outState))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel.restoreState(BundleWrapper.Base(savedInstanceState))
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/wrappers/LiveDataWrapper.kt
@@ -0,0 +1,31 @@
+package ru.easycode.zerotoheroandroidtdd.wrappers
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import ru.easycode.zerotoheroandroidtdd.UiState
+
+interface LiveDataWrapper {
+    fun liveData(): LiveData<UiState>
+    fun saveState(bundleWrapper: BundleWrapper.SaveState)
+    fun update(value: UiState)
+
+    class Base() : LiveDataWrapper {
+        private val liveData: MutableLiveData<UiState> = MutableLiveData()
+
+        override fun liveData(): LiveData<UiState> {
+            return liveData
+        }
+
+        override fun saveState(bundleWrapper: BundleWrapper.SaveState): Unit = synchronized(this) {
+            liveData.value?.let { bundleWrapper.saveState(it) }
+        }
+
+        override fun update(value: UiState) = synchronized(this) {
+            liveData.value = value
+        }
+    }
+}
+
+interface SaveState {
+    fun saveState(bundleWrapper: BundleWrapper.SaveState)
+}
