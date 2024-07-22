3bda682b054da3a3bacf87ed4b1feabb818fcf28
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-07-22 16:22:06 +0300

    feat: add input and display list of strings
    Adds functionality to input strings and display them in a recycler view.
    Also includes saving and restoring the state
     of the list.

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,10 +7,35 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainAdapter: MainRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mainAdapter = MainRecyclerAdapter.Default()
+        with(binding.recyclerView) {
+            adapter = mainAdapter
+        }
+        binding.actionButton.setOnClickListener {
+            val inputText = binding.inputEditText.text
+            if (inputText == null) return@setOnClickListener
+
+            mainAdapter.addItem(inputText.toString())
+            binding.inputEditText.text?.clear()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val bundleWrapper = BundleWrapper.Default(outState)
+        mainAdapter.saveState(bundleWrapper)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val bundleWrapper = BundleWrapper.Default(savedInstanceState)
+        mainAdapter.restoreState(bundleWrapper)
     }
 }
